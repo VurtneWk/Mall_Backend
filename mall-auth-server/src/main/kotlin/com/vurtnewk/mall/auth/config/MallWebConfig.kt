c5ed4cbc2dbf0ba09485d3e10f2d563dbe36e2a5
vurtnewk <vurtnewk@163.com>
2025-01-20 15:53:54 +0800

    212、商城业务-认证服务-好玩的验证码倒计时

---
+++ mall-auth-server/src/main/kotlin/com/vurtnewk/mall/auth/config/MallWebConfig.kt
@@ -0,0 +1,29 @@
+package com.vurtnewk.mall.auth.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+/**
+ * Spring MVC 配置
+ * @author   vurtnewk
+ * @since    2025/1/20 15:43
+ */
+@Configuration
+class MallWebConfig : WebMvcConfigurer {
+
+    /**
+     * 视图映射
+     * 代替下面这种方法
+     * ```kotlin
+     *     @GetMapping("/login.html")
+     *     fun loginPage() = "login"
+     * ```
+     */
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+//        super.addViewControllers(registry)
+        registry.addViewController("/login.html").setViewName("login")
+        registry.addViewController("/reg.html").setViewName("reg")
+    }
+
+}
---
+++ mall-auth-server/src/main/kotlin/com/vurtnewk/mall/auth/controller/LoginController.kt
@@ -0,0 +1,19 @@
+package com.vurtnewk.mall.auth.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+/**
+ * 登录
+ * @author   vurtnewk
+ * @since    2025/1/20 12:29
+ */
+@Controller
+class LoginController {
+
+//    @GetMapping("/login.html")
+//    fun loginPage() = "login"
+//
+//    @GetMapping("/reg.html")
+//    fun regPage() = "reg"
+}
