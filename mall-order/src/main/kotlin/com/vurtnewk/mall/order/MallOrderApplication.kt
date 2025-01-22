2bcc5fd56165de1d790140f8400bd4835123c82e
vurtnewk <vurtnewk@163.com>
2025-01-23 05:31:53 +0800

    255、商城业务-消息队列-SpringBoot整合RabbitMQ

--- mall-order/src/main/kotlin/com/vurtnewk/mall/order/MallOrderApplication.kt
+++ mall-order/src/main/kotlin/com/vurtnewk/mall/order/MallOrderApplication.kt
@@ -1,9 +1,11 @@
 package com.vurtnewk.mall.order
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 class MallOrderApplication
--- mall-order/src/test/kotlin/com/vurtnewk/mall/order/MallOrderApplicationTests.kt
+++ mall-order/src/test/kotlin/com/vurtnewk/mall/order/MallOrderApplicationTests.kt
@@ -1,10 +1,23 @@
 package com.vurtnewk.mall.order
 
 import org.junit.jupiter.api.Test
+import org.springframework.amqp.core.AmqpAdmin
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MallOrderApplicationTests {
+class MallOrderApplicationTests(
+    private val amqpAdmin: AmqpAdmin
+) {
+
+
+    /**
+     * 1. 如何创建 exchange 、 queue 、 binding
+     * 2. 如何收发消息
+     */
+    fun testRabbit() {
+
+    }
+
 
     @Test
     fun contextLoads() {
