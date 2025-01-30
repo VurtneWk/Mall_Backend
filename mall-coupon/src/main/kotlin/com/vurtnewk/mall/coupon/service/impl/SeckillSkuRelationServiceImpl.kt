183076a53ef11f5e3878ee961a8155e7f4ef7119
vurtnewk <vurtnewk@163.com>
2025-01-30 23:15:42 +0800

    309、商城业务-订单服务-收单

--- mall-coupon/src/main/kotlin/com/vurtnewk/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.kt
+++ mall-coupon/src/main/kotlin/com/vurtnewk/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.kt
@@ -1,25 +1,23 @@
 package com.vurtnewk.mall.coupon.service.impl
 
-import org.springframework.stereotype.Service
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
-import com.baomidou.mybatisplus.core.metadata.IPage
+import com.baomidou.mybatisplus.extension.kotlin.KtQueryChainWrapper
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
 import com.vurtnewk.common.utils.PageUtils
-import com.vurtnewk.common.utils.Query
-
+import com.vurtnewk.common.utils.ext.pageUtils
+import com.vurtnewk.common.utils.ext.toPage
 import com.vurtnewk.mall.coupon.dao.SeckillSkuRelationDao
 import com.vurtnewk.mall.coupon.entity.SeckillSkuRelationEntity
 import com.vurtnewk.mall.coupon.service.SeckillSkuRelationService
+import org.springframework.stereotype.Service
 
 
 @Service("seckillSkuRelationService")
-class SeckillSkuRelationServiceImpl : ServiceImpl<SeckillSkuRelationDao, SeckillSkuRelationEntity>() , SeckillSkuRelationService {
+class SeckillSkuRelationServiceImpl : ServiceImpl<SeckillSkuRelationDao, SeckillSkuRelationEntity>(), SeckillSkuRelationService {
 
     override fun queryPage(params: Map<String, Any>): PageUtils {
-        val page = this.page(
-            Query<SeckillSkuRelationEntity>().getPage(params),
-            QueryWrapper<SeckillSkuRelationEntity>()
-        )
-        return PageUtils(page)
+        val promotionSessionId = params["promotionSessionId"] as? String
+        return KtQueryChainWrapper(SeckillSkuRelationEntity::class.java)
+            .eq(!promotionSessionId.isNullOrBlank(), SeckillSkuRelationEntity::promotionSessionId, promotionSessionId)
+            .toPage(params).pageUtils()
     }
 }
