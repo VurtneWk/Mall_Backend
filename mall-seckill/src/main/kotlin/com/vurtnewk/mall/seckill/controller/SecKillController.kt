6c97f8815ca85fd9e46aed38df9e2d441b37803b
vurtnewk <vurtnewk@163.com>
2025-02-07 12:19:08 +0800

    324、商城业务-秒杀服务-秒杀页面完成

--- mall-seckill/src/main/kotlin/com/vurtnewk/mall/seckill/controller/SecKillController.kt
+++ mall-seckill/src/main/kotlin/com/vurtnewk/mall/seckill/controller/SecKillController.kt
@@ -3,23 +3,26 @@ package com.vurtnewk.mall.seckill.controller
 import com.vurtnewk.common.utils.R
 import com.vurtnewk.common.utils.ext.logInfo
 import com.vurtnewk.mall.seckill.service.SecKillService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
 /**
  *
  * @author   vurtnewk
  * @since    2025/1/31 21:32
  */
-@RestController
+@Controller
 class SecKillController(private val secKillService: SecKillService) {
 
     /**
      * 返回当前时间可以参与的秒杀商品信息
      */
     @GetMapping("/currentSecKillSkus")
+    @ResponseBody
     fun getCurrentSecKillSkus(): R {
         logInfo("getCurrentSecKillSkus")
         val list = secKillService.getCurrentSecKillSkus()
@@ -30,6 +33,7 @@ class SecKillController(private val secKillService: SecKillService) {
      * 查询指定 skuId 的秒杀信息
      */
     @GetMapping("/sku/secKill/{skuId}")
+    @ResponseBody
     fun getSkuSecKillInfo(@PathVariable("skuId") skuId: Long): R {
         val info = secKillService.getSkuSecKillInfo(skuId)
         return R.ok().putData(info)
@@ -40,9 +44,10 @@ class SecKillController(private val secKillService: SecKillService) {
         @RequestParam("killId") killId: String,
         @RequestParam("key") key: String,
         @RequestParam("num") num: Int,
-    ): R {
+        model: Model,
+    ): String {
         val orderSn = secKillService.kill(killId, key, num)
-        return R.ok().putData(orderSn)
+        model.addAttribute("orderSn", orderSn)
+        return "success"
     }
-
 }
